47d19715cb9bb58becff6a0df67d403b00d766cf
BenWhite <958255724@qq.com>
2018-03-01 10:32:15 +0800

    补充README

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # django_blog
-a blog designed by Django
+## 使用Python3.6 + Django2.0.2 + Bootstrap开发的个人博客，具有归档、标签和haystack搜索功能。
+
--- blog/urls.py
+++ blog/urls.py
@@ -21,7 +21,6 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 
-
 urlpatterns = [
     path(r'admin/', admin.site.urls),
     path(r'', IndexView.as_view(), name='index'),
