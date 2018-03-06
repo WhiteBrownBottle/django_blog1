e0768d0a42cedb3dac08c3f35df0ed16c43b8ef5
BenWhite <958255724@qq.com>
2018-03-06 08:34:23 +0800

    为部署做准备

--- blog/settings.py
+++ blog/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '1ec3iw*le@qtujn-#(pt3y!c*+2ak+@#o1uhh^t-o#n*uirbl*'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost', '.baicaicoder.online']
 
 
 # Application definition
@@ -130,9 +130,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),
-]
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+# STATICFILES_DIRS = [
+#     os.path.join(BASE_DIR, 'static'),
+# ]
 
 
 PAGINATION_SETTINGS = {
