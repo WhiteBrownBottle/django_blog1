d7fc8351c7534a86c31dd31ce6b2757f1e3ede12
BenWhite <958255724@qq.com>
2018-03-06 21:04:58 +0800

    恢复线下部署的设置

--- blog/settings.py
+++ blog/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '1ec3iw*le@qtujn-#(pt3y!c*+2ak+@#o1uhh^t-o#n*uirbl*'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', 'localhost', '.baicaicoder.online']
+ALLOWED_HOSTS = []
 
 
 # Application definition
@@ -130,10 +130,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-# STATICFILES_DIRS = [
-#     os.path.join(BASE_DIR, 'static'),
-# ]
+#STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
 
 
 PAGINATION_SETTINGS = {
